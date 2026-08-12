18158160e95e687313120699bba7eb81cef666bb
brandon <rparks1@babson.edu>
2026-08-12 17:35:38 -0400

    Isolate CocoaPods from EAS Bundler environment

--- script/prepare_eas_ios.sh
+++ script/prepare_eas_ios.sh
@@ -9,6 +9,7 @@ fi
 pod_source="$({
   ruby -e 'spec = Gem::Specification.find_by_name("cocoapods", "1.16.2"); print File.join(spec.full_gem_path, "bin", "pod")'
 })"
+ruby_binary="$(command -v ruby)"
 
 if [[ ! -f "$pod_source" ]]; then
   echo "CocoaPods executable was not found at $pod_source" >&2
@@ -16,5 +17,10 @@ if [[ ! -f "$pod_source" ]]; then
 fi
 
 sudo mkdir -p /usr/local/bin
-sudo ln -sf "$pod_source" /usr/local/bin/pod
+sudo tee /usr/local/bin/pod >/dev/null <<EOF
+#!/usr/bin/env bash
+unset BUNDLE_BIN_PATH BUNDLE_GEMFILE RUBYLIB RUBYOPT
+exec "$ruby_binary" "$pod_source" "\$@"
+EOF
+sudo chmod 755 /usr/local/bin/pod
 /usr/local/bin/pod --version
