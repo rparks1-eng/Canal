6699689aca7873f86cece8d3d34c29d799f05e99
brandon <rparks1@babson.edu>
2026-08-12 17:46:03 -0400

    Make EAS CocoaPods launcher self contained

--- script/prepare_eas_ios.sh
+++ script/prepare_eas_ios.sh
@@ -10,6 +10,8 @@ pod_source="$({
   ruby -e 'spec = Gem::Specification.find_by_name("cocoapods", "1.16.2"); print File.join(spec.full_gem_path, "bin", "pod")'
 })"
 ruby_binary="$(command -v ruby)"
+gem_home="$(ruby -e 'print Gem.dir')"
+gem_path="$(ruby -e 'print Gem.path.join(":")')"
 pod_target="$(ruby -e 'print File.join(Gem.bindir, "pod")')"
 
 if [[ ! -f "$pod_source" ]]; then
@@ -20,9 +22,11 @@ fi
 mkdir -p "$(dirname "$pod_target")"
 rm -f "$pod_target"
 tee "$pod_target" >/dev/null <<EOF
-#!/usr/bin/env bash
+#!/bin/bash
 unset BUNDLE_BIN_PATH BUNDLE_GEMFILE RUBYLIB RUBYOPT
 export COCOAPODS_NO_BUNDLER=1
+export GEM_HOME="$gem_home"
+export GEM_PATH="$gem_path"
 exec "$ruby_binary" "$pod_source" "\$@"
 EOF
 chmod 755 "$pod_target"
@@ -32,3 +36,4 @@ if [[ "$resolved_pod" != "$pod_target" ]]; then
   exit 1
 fi
 pod --version
+env -i PATH="$(dirname "$pod_target")" "$pod_target" --version
